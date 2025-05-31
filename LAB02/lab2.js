73c55b087ed29eec29841da530d9b5b887f95908
Zeel0911 <zeel.rana1109@gmail.com>
2025-05-30 20:14:45 -0400

    first step of code

---
+++ LAB02/lab2.js
@@ -0,0 +1,12 @@
+// This is a simple Rock, Paper, Scissors game 
+const prompt = require('prompt');
+
+console.log("\nWelcome to the Rock, Paper, Scissors game....!!!\n");
+console.log("To start your game, please select one of the following options: ROCK, PAPER, SCISSORS\n");
+
+// Define the valid choices for the game
+const choices = ['ROCK', 'PAPER', 'SCISSORS'];
+
+// Initialize the prompt for user input
+prompt.start();
+
