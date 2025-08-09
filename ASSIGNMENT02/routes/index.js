1f901a84b7f18f02b06172c72f66e63716095dbc
Zeel0911 <zeel.rana1109@gmail.com>
2025-08-08 21:04:23 -0400

    designing home page

--- ASSIGNMENT02/routes/index.js
+++ ASSIGNMENT02/routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var Task = require('../models/task');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/* GET home page with tasks from DB */
+router.get('/', async function(req, res, next) {
+  try {
+    const tasks = await Task.find();
+    res.render('index', { title: 'My To-Do List App', tasks });
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
